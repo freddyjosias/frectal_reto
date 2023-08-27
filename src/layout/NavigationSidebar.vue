dbd36236220229b5d9b5a65a64c19551af3ba8c9
freddyjosias <gerson123tuesta@gmail.com>
2023-08-27 10:30:06 -0500

    Add tailwind

---
+++ src/layout/NavigationSidebar.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'NavigationSidebar'
+}
+</script>
+
+<template>
+  <nav class="bg-blue-200">
+    <router-link :to="{ name: 'countriesList' }">Home</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link>
+    <router-link :to="{ name: 'help' }">Help</router-link>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+  height: 100%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'countriesList',
+    component: () => import('../views/CountiesListView.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/help',
+    name: 'help',
+    component: () => import('../views/HelpView.vue')
   }
 ]
 
